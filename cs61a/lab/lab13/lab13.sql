a7ea75fd1f138322c174d2f44cd261759de49740
William Sosropartono <wcsosropartono@gmail.com>
2022-10-28 18:14:44 -0700

    SQL exercises completed

--- cs61a/lab/lab13/lab13.sql
+++ cs61a/lab/lab13/lab13.sql
@@ -2,24 +2,26 @@
 
 
 CREATE TABLE bluedog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet FROM students WHERE color = 'blue' AND pet = 'dog';
 
 CREATE TABLE bluedog_songs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet, song FROM students WHERE color = 'blue' AND pet = 'dog';
 
+
+-- Group by organizes the projected tables into groups, the groups have a count, but typically what is displayed is some row from that group, in which only the group in which the expression evaluates to true is shown
 
 CREATE TABLE smallest_int_having AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time, smallest FROM students GROUP BY smallest HAVING count(smallest) = 1;
 
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.pet, a.song, a.color, b.color FROM students AS a, students AS b WHERE a.pet = b.pet AND a.song = b.song AND a.time <> b.time;
 
 
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT seven FROM students as a, numbers as b WHERE number = 7 AND b."7" = "True" AND a.time = b.time LIMIT 10;
 
 
 CREATE TABLE avg_difference AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT round(avg(abs(number-smallest)))  FROM students;
 
